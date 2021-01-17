6833979f642e7a31e3507fc9f42295919fb14e12
doctor11th <13305712209@126.com>
2021-01-17 10:57:51 +0800

    Js

--- Control_Part/Js-Part/Control_Login.js
+++ Control_Part/Js-Part/Control_Login.js
@@ -1,5 +1,9 @@
 function sub() {
     let a = document.getElementById("userName");
     let b = document.getElementById("password");
-    if (a.value == "" || b.value == "") alert("用户名或密码不能为空");
+    if (a.value == "" || b.value == "") {
+        alert("用户名或密码不能为空");
+        return false;
+    }
+    return true;
 }
